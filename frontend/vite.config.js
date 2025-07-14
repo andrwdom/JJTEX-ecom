f7f2bc68b15391e32670135aae0b353f69364389
andrwdom <m.andrewdominic9b@gmail.com>
2025-07-14 11:19:38 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port:5173}
+  server: { port: 5173 },
+  preview: {
+    allowedHosts: ['jjtextiles.com', 'www.jjtextiles.com', 'admin.jjtextiles.com', 'api.jjtextiles.com']
+  }
 })
