0bd7003f292e8c086391505ea194944e58524829
andrwdom <m.andrewdominic9b@gmail.com>
2025-07-18 13:39:07 +0530

    a

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -6,7 +6,10 @@ const userRouter = express.Router();
 
 userRouter.post('/register',registerUser)
 userRouter.post('/login',loginUser)
-userRouter.post('/admin',adminLogin)
+userRouter.post('/admin', (req, res, next) => {
+  console.log('POST /api/user/admin hit', req.method, req.body);
+  next();
+}, adminLogin)
 userRouter.get('/info', verifyToken, getUserInfo);
 
 export default userRouter;
