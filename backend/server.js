45e3c25d17091179d534e46b3db01f2ef777636f
andrwdom <m.andrewdominic9b@gmail.com>
2025-07-18 16:00:44 +0530

    w

--- backend/server.js
+++ backend/server.js
@@ -171,8 +171,8 @@ app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 
 // middlewares
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use('/uploads', express.static('uploads'))
 
 // api endpoints
